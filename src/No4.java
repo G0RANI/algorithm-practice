b6d52c75ec8026f155459c4c9765aa88a78e367a
JEONGWOO PARK <pjw0405@naver.com>
2023-04-20 23:45:09 +0900

    day3

---
+++ src/No4.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class No4 {
+
+    private static final List<String> inputs = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int input = in.nextInt();
+        while (input > 0) {
+            input--;
+            inputs.add(in.next());
+        }
+        for (String input2 : inputs) {
+            System.out.println(solution(input2));
+        }
+
+    }
+
+    public static String solution(String input) {
+        StringBuilder result = new StringBuilder();
+        int lenth = input.length();
+        char[] chars = input.toCharArray();
+        while (lenth > 0) {
+            char a = chars[lenth-1];
+            result.append(a);
+            lenth--;
+        }
+        return result.toString();
+    }
+}
