29658bc08b18ee557234b6ce88ee8cad888b5d0f
JEONGWOO PARK <pjw0405@naver.com>
2023-06-22 19:53:06 +0900

    algo-day16

---
+++ src/programers/No26.java
@@ -0,0 +1,16 @@
+package programers;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class No26 {
+    // https://school.programmers.co.kr/learn/courses/30/lessons/86051
+    public int solution(int[] numbers) {
+        int answer = 0;
+        String target = Arrays.stream(numbers).mapToObj(String::valueOf).collect(Collectors.joining());
+        for(int i = 0; i< 10; i++) {
+            if(!target.contains(String.valueOf(i))) answer += i;
+        }
+        return answer;
+    }
+}
